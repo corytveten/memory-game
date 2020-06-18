8c9bec6ab83b3f5e29b20bd7371df963c23c6d79
corytveten <corytveten@gmail.com>
2020-06-18 15:38:04 -0700

    refactor

--- scripts.js
+++ scripts.js
@@ -10,25 +10,34 @@ function flipCard() {
         //first click
         hasFlippedCard = true;
         firstCard = this;
-    } else {
-        //second click
-        hasFlippedCard = false;
-        secondCard = this;
     
-        //do cards match
-        if (firstCard.dataset.framework === secondCard.dataset.framework) {
-            //it's a match
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            setTimeout(() => {
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-            }, 1500)
-        }
-
-        console.log('function was exe');
+        return;
     }
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unFlipCards();
+
+}
+
+function disableCards() {
+    //it's a match
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unFlipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        }, 1500)
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
